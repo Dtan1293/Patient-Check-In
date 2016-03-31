f36aec148c31150d5022977e088ae73f42075a10
Dtan1293 <Dtan1293@gmail.com>
2016-03-31 01:24:16 -0700

    Pushing changes
    Minutes looks better, finally has the ability to remove items from the
    database!

--- admin.js
+++ admin.js
@@ -1,7 +1,6 @@
+var ref = new Firebase("https://medicallogin2193.firebaseio.com/patients");
 $(function() { //wait for the DOM to be ready!
-	var ref = new Firebase("https://medicallogin2193.firebaseio.com/patients");
 	var count = 1;
-
 	ref.on("child_added", function(data) {
 		var patient_name = data.child("patient").val();
 		var check_in_time = data.child("CheckIn").val();
@@ -9,7 +8,7 @@ $(function() { //wait for the DOM to be ready!
 		var reason = data.child("Reason").val();
 		var should_display = data.child("display").val();
 
-		if(should_display === true) {
+		if(should_display === "true") {
 			$("#patient_display_information").append("<tr><td>" + patient_name + 
 												 "</td><td>" + check_in_time + 
 												 "</td><td>" + provider + 
@@ -22,9 +21,12 @@ $(function() { //wait for the DOM to be ready!
 	
 	// Attach an asynchronous callback to when a child is removed! Just refreshes the page!
 	//Needs to be updated!
+	/*
 	ref.on("child_removed", function(data) {
 		location.reload();
 	});
+	
+	*/
 
 	$("#destroy_database").click(function() {
 		if(confirm("Are you sure?") == true) {
@@ -38,6 +40,20 @@ $(function() { //wait for the DOM to be ready!
 });
 
 function strikeOutPatient(patient_number) {
+	var count = 0;
 	$("tr").eq(patient_number).addClass("strikeOut");
+	var string = "https://medicallogin2193.firebaseio.com/patients/";
 
+	ref.once("value", function(snapshotparent) {
+		snapshotparent.forEach(function(childSnapshot) {
+			console.log(childSnapshot.key());
+			if (count === patient_number - 1) {
+				string += childSnapshot.key();
+			}
+			count++;
+		});
+	});
+	var remove_node = new Firebase(string);
+	//remove_node.remove(); //this is to forever erase the data!
+	remove_node.update({display: "false"});
 }
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 $(function() {
 	//temp solution for storing email addresses! This will be put on the database!
 	var email_address = ["Dtan1293@gmail.com", "UWDtan1293@gmail.com"];
-	
+
 	$(document).ready(function() {
 		$("#submit_button").click(function() {
 			clicked();
@@ -12,7 +12,7 @@ $(function() {
 		});
 
 		//if user clicks anywhere on the website, it will direct towards input id="name"
-		$("html").click(function(){
+		$("html").one("click", function(){
 			$("#name").focus();
 		});
 
@@ -23,6 +23,7 @@ $(function() {
 			var patient_name = patient_submitted_data[0];
 			var provider = patient_submitted_data[1];
 			var reason_for_visit = patient_submitted_data[2];
+			var provider_email_address_index = patient_submitted_data[3];
 
 			//grab the current time so patient doesn't have to enter it
 			var d = new Date();
@@ -32,12 +33,22 @@ $(function() {
 			var AM_PM = " AM";
 			if(d.getHours() > 12) {
 				hours = d.getHours() - 12;
+				if(hours === 0)
+					hours = 12;
 				AM_PM = " PM";
 			} else {
 				hours = d.getHours();
 			}
 
-			var time = hours + ":" + d.getMinutes() + ":" + d.getSeconds() + AM_PM;
+			//making sure anything less than 10 minutes doesn't look werid
+			var minutes = "";
+			if(d.getMinutes() < 10) {
+				minutes += "0" + d.getMinutes();
+			} else {
+				minutes += d.getMinutes();
+			}
+
+			var time = hours + ":" + minutes + ":" + d.getSeconds() + AM_PM;
 			//storing information on the database!
 			var ref = new Firebase("https://medicallogin2193.firebaseio.com/");
 			var usersRef = ref.child("patients");
@@ -47,7 +58,7 @@ $(function() {
 			 	CheckIn: time,
 			 	Provider: provider,
 			 	Reason: reason_for_visit,
-			 	display: true
+			 	display: "true"
 			});
 
 			alert("Done! Please press Ok to complete your check-in");
@@ -56,7 +67,7 @@ $(function() {
 			$.post( 
 			  "send.php",
 			  { 
-			  	provider_email: "Dtan1293@gmail.com",
+			  	provider_email: email_address[provider_email_address_index],
 			  	patient: patient_name 
 			  })
 			.done(function( data ) {
@@ -75,11 +86,17 @@ $(function() {
 
 			//grab provider radio button:
 			var provider;
+			var provider_index;
 			if($( "input:radio[name=Provider]:checked" ).length === 0) {
 				alert("No provider selected!");
 				return;
 			} else {
 				provider = $( "input:radio[name=Provider]:checked" ).val();
+				$("input:radio[name=Provider]").each(function(i) {
+				   if (this.checked) {
+				   		provider_index = i;
+				   }
+				});
 			}
 
 			//grab the reason for visit radio button:
@@ -90,8 +107,7 @@ $(function() {
 			} else {
 				reason_for_visit = $( "input:radio[name=RFV]:checked" ).val();
 			}
-			return info = [patient_name, provider, reason_for_visit];
+			return info = [patient_name, provider, reason_for_visit, provider_index];
 		}
-
 	});
 });
